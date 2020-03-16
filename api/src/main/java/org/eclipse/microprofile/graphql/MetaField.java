790d50e8b841bce881301e7e20694e71f6f92a92
mhe <markus.herhoffer@exentra.de>
2020-03-16 11:18:00 +0100

    Correct retention to RUNTIME

--- api/src/main/java/org/eclipse/microprofile/graphql/MetaField.java
+++ api/src/main/java/org/eclipse/microprofile/graphql/MetaField.java
@@ -1,52 +1,50 @@
 package org.eclipse.microprofile.graphql;
 
 import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.CLASS;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
 public class MetaField {
 
-	private String name;
-	private Class<?> type;
-	private String description;
+    private String name;
+    private Class<?> type;
+    private String description;
 
-	public MetaField(String name, Class<?> type) {
-		this.name = name;
-		this.type = type;
-	}
+    public MetaField(String name, Class<?> type) {
+	this.name = name;
+	this.type = type;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+	return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+	this.name = name;
+    }
 
-	public Class<?> getType() {
-		return type;
-	}
+    public Class<?> getType() {
+	return type;
+    }
 
-	public void setType(Class<?> type) {
-		this.type = type;
-	}
+    public void setType(Class<?> type) {
+	this.type = type;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+	return description;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public void setDescription(String description) {
+	this.description = description;
+    }
 
-	@Retention(CLASS)
-	@Target(METHOD)
-	public static @interface MetaFields {
-		
-		Class<? extends MetaFieldType> targetType();
-	}
+    @Retention(RUNTIME)
+    @Target(METHOD)
+    public static @interface MetaFields {
+
+	Class<? extends MetaFieldType> targetType();
+    }
 }
